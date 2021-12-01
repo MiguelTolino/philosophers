b578ea34c25823854149ce9a607af2100715c4fd
MiguelTolino <mmateo-t@student.42madrid.com>
2021-12-01 14:32:39 +0100

    Print_log and diff_time functions added Print_log is a function that print the state of a philosopher -Diff_time returns the difference between two times

---
+++ srcs/print_log.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_log.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 11:31:57 by mmateo-t          #+#    #+#             */
+/*   Updated: 2021/12/01 14:29:05 by mmateo-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void print_log(char *str, long timestamp, int philo_nb, t_struct p)
+{
+	pthread_mutex_lock(&p.print_mutex);
+	printf("%li %i %s\n", timestamp, philo_nb, str);
+	pthread_mutex_unlock(&p.print_mutex);
+}
